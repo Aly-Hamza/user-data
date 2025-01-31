dae9426bb26fccbe1f19047b470f8a0adeead725
Malik Ali <mac@macs-MacBook-Pro.local>
2025-01-31 20:04:58 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './component/navbar'
+import UserDetails from './component/UserDetails'
+const App = () => {
+  return (
+    <>
+     <Navbar/>
+     <UserDetails/>
+    </>
+  )
+}
+
+export default App
